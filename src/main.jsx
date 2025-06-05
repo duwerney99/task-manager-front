3bf95c6a3ecb1c3a4ebf94599d4e5944ece91ba3
Duwerney99 <santiagohernan99dez@gmail.com>
2025-06-04 19:27:49 -0500

    chore: se agrego dependencia notistack

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,22 @@ import { Provider } from 'react-redux'
 import store from './app/store.js';
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import theme from './theme/index.js';
+import { SnackbarProvider } from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SnackbarProvider
+          maxSnack={3}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          autoHideDuration={3000}
+        >
           <App />
-        </ThemeProvider>
+        </SnackbarProvider>
+        
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
 )
